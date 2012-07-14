3d7163a9c782eab00927aff41d0ac168a8ea5fc6
Jason Zaugg <jzaugg@gmail.com>
2012-07-14 09:23:45 +0200

    Make it work

--- README.md
+++ README.md
@@ -5,3 +5,11 @@ An sbt plugin to list all dependencies of an SBT project.
 Cheerleading and 1% code by Mike Slinn.
 
 Daily usage by millions.
+
+## Usage
+
+Add setting to your project:
+
+    com.micronautics.dependencyReport.DependencyReport.dependencyReportSettings
+
+This adds the tasks `compile:dependency-report` and `test:dependency-report`.
--- src/main/scala/dependencyReport.scala
+++ src/main/scala/dependencyReport.scala
@@ -1,25 +1,32 @@
+package com.micronautics.dependencyReport
+
 import sbt._
+import sbt.Keys._
 
 object DependencyReport extends Plugin {
   lazy val dependencyReport = TaskKey[String]("dependency-report")
 
-  dependencyReport <<= (externalDependencyClasspath in Compile, streams) map {
-    (cp: Seq[Attributed[File]], streams) =>
-       val report = cp.map {
-         attributed =>
-           attributed.get(Keys.moduleID.key) match {
-             case Some(moduleId) => "%40s %20s %10s %10s".format(
-               moduleId.organization,
-               moduleId.name,
-               moduleId.revision,
-               moduleId.configurations.getOrElse("")
-             )
+  val dependencyReportSettings: Seq[Setting[_]] = inConfig(Compile)(unscopedSettings) ++ inConfig(Test)(unscopedSettings)
 
-             case None => // unmanaged JAR
-               attributed.data.getAbsolutePath
-           }
-       }.mkString("\n")
-     streams.log.info(report)
-     report
-  }
+  val unscopedSettings: Seq[Setting[_]] = Seq(
+    dependencyReport <<= (externalDependencyClasspath in Compile, streams) map {
+      (cp: Seq[Attributed[File]], streams) =>
+         val report = cp.map {
+           attributed =>
+             attributed.get(Keys.moduleID.key) match {
+               case Some(moduleId) => "%40s %20s %10s %10s".format(
+                 moduleId.organization,
+                 moduleId.name,
+                 moduleId.revision,
+                 moduleId.configurations.getOrElse("")
+               )
+
+               case None => // unmanaged JAR
+                 attributed.data.getAbsolutePath
+             }
+         }.mkString("\n")
+       streams.log.info(report)
+       report
+    }
+  )
 }
